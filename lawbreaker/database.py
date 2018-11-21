b0046f46dd9f7f32e034b860154cdd3af3ca2757
Harshavardhan Rangan <hvardhan.r@gmail.com>
2018-11-21 20:22:16 +0530

    moved to postgres for data permanency on heroku

--- lawbreaker/database.py
+++ lawbreaker/database.py
@@ -1,4 +1,6 @@
-import sqlite3
+import os
+import psycopg2
+
 from datetime import datetime
 
 from lawbreaker.exceptions import NoResultsFound
@@ -6,24 +8,27 @@ from lawbreaker.exceptions import NoResultsFound
 
 class Database(object):
     def __init__(self):
-        self.conn = sqlite3.connect('lawbreaker.db')
-        self.cursor = self.conn.cursor()
+        database_url = os.environ['DATABASE_URL']
+        conn = psycopg2.connect(database_url, sslmode='require')
+        conn.set_session(autocommit=True)
+        self.cursor = conn.cursor()
+
         self.cursor.execute('''CREATE TABLE IF NOT EXISTS characters
-                                   (character_id text,
+                                   (character_id text PRIMARY KEY UNIQUE,
                                     character_json text,
-                                    timestamp text)''')
-        self.conn.commit()
+                                    timestamp timestamp)''')
 
     def select(self, character_id):
-        result = self.cursor.execute('''SELECT character_json FROM characters WHERE character_id=?''',
-                                     (character_id,))
-        try:
-            return result.fetchone()[0]
-        except TypeError:
+        self.cursor.execute("SELECT character_json FROM characters WHERE character_id=%s", (character_id,))
+        result = self.cursor.fetchone()
+        if result is None:
             raise NoResultsFound
+        else:
+            self.cursor.execute("UPDATE characters SET timestamp=%s where character_id=%s",
+                                (datetime.utcnow(), character_id))
+            return result[0]
 
     def insert(self, character_id, character_json):
-        self.cursor.execute('''INSERT INTO characters(character_id, character_json, timestamp)
-                               VALUES (?, ?, ?)''',
-                            (character_id, character_json, str(datetime.utcnow())))
-        self.conn.commit()
+        self.cursor.execute("""INSERT INTO characters(character_id, character_json, timestamp)
+                               VALUES (%s, %s, %s)""",
+                            (character_id, character_json, datetime.utcnow()))
