9aa58999ce0531f1afcd3e1912fa8afbd9cd81de
Eric W <eric@taedium.com>
2018-11-22 23:40:50 -0800

    only make a random name if it's actually wanted.

--- lawbreaker/knave.py
+++ lawbreaker/knave.py
@@ -7,8 +7,8 @@ from names import Name
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process some integers.")
     parser.add_argument("--level", dest="level", action="store", type=int, default=1)
-    parser.add_argument('--random-name', action='store_const', dest='randomName', const=Name.get())
+    parser.add_argument('--random-name', action='store_const', dest='randomName', const=True)
     parser.add_argument("name", action="store", default=["_"*20], nargs='*')
     args = parser.parse_args()
-    char = Character(name=args.randomName and args.randomName or " ".join(args.name), level=args.level)
+    char = Character(name=args.randomName and Name.get() or " ".join(args.name), level=args.level)
     print char
