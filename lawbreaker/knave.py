351ba96280a990e8be6c74e83a6b40242420b8e6
Harshavardhan Rangan <hvardhan.r@gmail.com>
2018-11-24 13:14:28 +0530

    --random-name and pos. names are now mutually exclusive

--- lawbreaker/knave.py
+++ lawbreaker/knave.py
@@ -7,8 +7,11 @@ from names import Name
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process some integers.")
     parser.add_argument("--level", dest="level", action="store", type=int, default=1)
-    parser.add_argument('--random-name', action='store_const', dest='randomName', const=True)
-    parser.add_argument("name", action="store", default=["_"*20], nargs='*')
+
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('--random-name', action='store_const', dest='randomName', const=True)
+    group.add_argument("name", action="store", default=["_"*20], nargs='*')
+
     args = parser.parse_args()
     char = Character(name=args.randomName and Name.get() or " ".join(args.name), level=args.level)
     print char
