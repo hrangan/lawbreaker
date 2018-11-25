43558f7b7124c4ccda7abc63935dea11294c6d64
Harshavardhan Rangan <hvardhan.r@gmail.com>
2018-11-25 12:09:08 +0530

    setting content-type for rest APIs

--- web/server.py
+++ web/server.py
@@ -20,8 +20,11 @@ def main():
     character = Character(name=Name.get())
     character_json = repr(character)
     db.insert(character.id, character_json)
-    return character_json if 'application/json' in request.headers.get('Accept', '') \
-        else template('web/templates/index', content=json.loads(character_json, object_pairs_hook=OrderedDict))
+    if 'application/json' in request.headers.get('Accept'):
+        response.content_type = 'application/json'
+        return character_json
+    else:
+        return template('web/templates/index', content=json.loads(character_json, object_pairs_hook=OrderedDict))
 
 
 @route('/favicon.ico')
@@ -35,13 +38,19 @@ def fetch_by_id(character_id):
         character_json = db.select(character_id)
     except NoResultsFound:
         response.status = 404
-        return None if ('application/json' in request.headers.get('Accept', '')) \
-            else template('web/templates/error404')
+        if 'application/json' in request.headers.get('Accept'):
+            response.content_type = 'application/json'
+            return None
+        else:
+            return template('web/templates/error404')
 
-    return character_json if ('application/json' in request.headers.get('Accept', '')) \
-        else template('web/templates/index',
-                      content=json.loads(character_json, object_pairs_hook=OrderedDict),
-                      permalink=True)
+    if 'application/json' in request.headers.get('Accept'):
+        response.content_type = 'application/json'
+        return character_json
+    else:
+        return template('web/templates/index',
+                        content=json.loads(character_json, object_pairs_hook=OrderedDict),
+                        permalink=True)
 
 
 @route('/static/<path:path>')
