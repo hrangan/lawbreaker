f14c04389cb572bd2bd1d42b0fe6fb2144715948
Harshavardhan Rangan <hvardhan.r@gmail.com>
2018-11-25 22:59:29 +0530

    creating new cursors for each query

--- lawbreaker/database.py
+++ lawbreaker/database.py
@@ -9,26 +9,30 @@ from lawbreaker.exceptions import NoResultsFound
 class Database(object):
     def __init__(self):
         database_url = os.environ['DATABASE_URL']
-        conn = psycopg2.connect(database_url, sslmode='require')
-        conn.set_session(autocommit=True)
-        self.cursor = conn.cursor()
+        self.conn = psycopg2.connect(database_url, sslmode='require')
+        self.conn.set_session(autocommit=True)
 
-        self.cursor.execute('''CREATE TABLE IF NOT EXISTS characters (character_id text PRIMARY KEY UNIQUE,
-                                                                      character_json text,
-                                                                      expiry timestamp)''')
-
-        self.cursor.execute('''DELETE FROM characters WHERE expiry < now()''')
+        cursor = self.conn.cursor()
+        cursor.execute('''CREATE TABLE IF NOT EXISTS characters (character_id text PRIMARY KEY UNIQUE,
+                                                                 character_json text,
+                                                                 expiry timestamp)''')
+        cursor.execute('''DELETE FROM characters WHERE expiry < now()''')
+        cursor.close()
 
     def select(self, character_id):
-        self.cursor.execute("SELECT character_json FROM characters WHERE character_id=%s", (character_id,))
-        result = self.cursor.fetchone()
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT character_json FROM characters WHERE character_id=%s", (character_id,))
+        result = cursor.fetchone()
         if result is None:
             raise NoResultsFound
         else:
-            self.cursor.execute("UPDATE characters SET expiry=%s where character_id=%s",
-                                (datetime.utcnow()+timedelta(days=30), character_id))
+            cursor.execute("UPDATE characters SET expiry=%s where character_id=%s",
+                           (datetime.utcnow()+timedelta(days=30), character_id))
             return result[0]
+        cursor.close()
 
     def insert(self, character_id, character_json):
-        self.cursor.execute("""INSERT INTO characters(character_id, character_json, expiry) VALUES (%s, %s, %s)""",
-                            (character_id, character_json, datetime.utcnow()+timedelta(days=2)))
+        cursor = self.conn.cursor()
+        cursor.execute("""INSERT INTO characters(character_id, character_json, expiry) VALUES (%s, %s, %s)""",
+                       (character_id, character_json, datetime.utcnow()+timedelta(days=2)))
+        cursor.close()
