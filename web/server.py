656bbdb6a5af03c6a24af5b59a62b99ee2031b38
Harshavardhan Rangan <hvardhan.r@gmail.com>
2018-11-25 22:39:16 +0530

    using waitress, a dedicated web server

--- web/server.py
+++ web/server.py
@@ -60,6 +60,6 @@ def callback(path):
 
 if __name__ == '__main__':
     if os.environ.get('APP_LOCATION') == 'heroku':
-        run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+        run(server="waitress", host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
     else:
-        run(host='localhost', port=8080, reloader=True, debug=True)
+        run(server="waitress", host='localhost', port=8080, reloader=True, debug=True)
