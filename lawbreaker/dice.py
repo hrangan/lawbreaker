11284d32e06a19169fcf5f74ac6e41436f2b99af
Harshavardhan Rangan <hvardhan.r@gmail.com>
2018-11-27 21:28:31 +0530

    cleaner implementation of the Dice class

--- lawbreaker/dice.py
+++ lawbreaker/dice.py
@@ -5,16 +5,13 @@ from exceptions import InvalidDiceFormat
 
 
 class Dice(object):
-    def __getattr__(self, key):
+    def roll(self, key):
         try:
-            count, sides = re.match('_([0-9]+)d([0-9]+)', key).groups()
+            count, sides = re.match('^([0-9]+)d([0-9]+)', key).groups()
         except AttributeError:
             raise InvalidDiceFormat(key)
 
-        def dice():
-            return [random.randint(1, int(sides)) for x in range(int(count))]
-
-        return dice
+        return [random.randint(1, int(sides)) for x in range(int(count))]
 
 
 dice = Dice()
