9c54d725fdfb3b32112a0a9fc18adc895e72f68d
Harshavardhan Rangan <hvardhan.r@gmail.com>
2018-12-01 11:33:01 +0530

    added a keepawake function for heroku

--- web/server.py
+++ web/server.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 import os
+import time
 import sys
 import json
+import urllib2
+import threading
 
 from collections import OrderedDict
+from datetime import datetime, timedelta
 from bottle import route, run, template, static_file, request, response, redirect, hook
 
 from lawbreaker.character import Character
@@ -17,12 +21,30 @@ static_root = os.path.abspath(os.path.split(sys.argv[0])[0]) + '/static'
 
 
 if os.environ.get('APP_LOCATION') == 'heroku':
+    # SSL Redirection
     # This doesn't work locally since there is no SSL certificate
     @hook('before_request')
     def ssl_redirect():
         """Redirect incoming http requests to https"""
         if request.get_header('X-Forwarded-Proto', 'http') != 'https':
             redirect(request.url.replace('http://', 'https://', 1), code=301)
+
+    if os.environ.get('KEEP_AWAKE', 'false').lower() == 'true':
+        # Keep-awake polling
+        # Sets up a daemon that polls https://lawbreaker.herokuapp.com every 25
+        # minutes to stop the dyno from sleeping
+        def keep_awake():
+            last_run = datetime.now()
+            while True:
+                if datetime.now() - last_run > timedelta(seconds=25*60):
+                    print 'Polling https://lawbreaker.herokuapp.com to keep it awake'
+                    urllib2.urlopen("https://lawbreaker.herokuapp.com")
+                    last_run = datetime.now()
+                time.sleep(60)
+
+        thread = threading.Thread(target=keep_awake)
+        thread.daemon = True
+        thread.start()
 
 
 @route('/')
