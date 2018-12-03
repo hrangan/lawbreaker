b94ae21cd3e27c6337c5dd1a6566769596b7c310
Harshavardhan Rangan <hvardhan.r@gmail.com>
2018-12-03 22:55:25 +0530

    Added security headers

--- lawbreaker/web/server.py
+++ lawbreaker/web/server.py
@@ -5,7 +5,7 @@ import json
 from collections import OrderedDict
 
 from requests import get
-from flask import Flask, render_template, abort, request, redirect
+from flask import Flask, render_template, abort, request, redirect, make_response
 
 from lawbreaker.character import Character
 from lawbreaker.names import Name
@@ -45,14 +45,28 @@ if os.environ.get('APP_LOCATION') == 'heroku':
         spawn_daemon(keep_awake, interval=25*60)
 
 
+def secure_template(*args, **kwargs):
+    response = make_response(render_template(*args, **kwargs))
+    response.headers['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains'
+    response.headers['Content-Security-Policy'] = (
+                                                   "default-src 'none'; "
+                                                   "font-src 'self' https://fonts.gstatic.com data;"
+                                                   "img-src 'self';"
+                                                   "object-src 'none';"
+                                                   "script-src 'none';"
+                                                   "style-src 'self' https://fonts.googleapis.com;"
+                                                   "frame-ancestors 'self'"
+                                                   )
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+    return response
+
+
 @app.route('/')
 def generate_random():
     character = Character(name=Name.get())
     character_json = repr(character)
     db.insert(character.id, character_json)
-    return render_template('index.html',
-                           content=json.loads(character_json, object_pairs_hook=OrderedDict))
-
+    return secure_template('index.html', content=json.loads(character_json, object_pairs_hook=OrderedDict))
 
 @app.route('/<character_id>/')
 @app.route('/<character_id>')
@@ -61,15 +75,14 @@ def fetch_by_id(character_id):
         character_json = db.select(character_id)
     except NoResultsFound:
         abort(404)
-
-    return render_template('index.html',
+    return secure_template('index.html',
                            content=json.loads(character_json, object_pairs_hook=OrderedDict),
                            permalink=True)
 
 
 @app.errorhandler(404)
 def error404(error):
-    return render_template('404.html'), 404
+    return secure_template('404.html'), 404
 
 
 def main():
