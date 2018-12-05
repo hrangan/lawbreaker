e2e4a8a2bf0536e4977eecd28752c7839ecc80de
Harshavardhan Rangan <hvardhan.r@gmail.com>
2018-12-05 21:38:51 +0530

    added a profiling utility

--- lawbreaker/web/utils.py
+++ lawbreaker/web/utils.py
@@ -1,4 +1,4 @@
-import time
+from time import time, sleep
 import threading
 
 from datetime import datetime, timedelta
@@ -11,8 +11,18 @@ def spawn_daemon(func, interval):
             if datetime.now() - last_run > timedelta(seconds=interval):
                 func()
                 last_run = datetime.now()
-            time.sleep(interval/20)
+            sleep(interval/20)
 
     thread = threading.Thread(target=_daemon)
     thread.daemon = True
     thread.start()
+
+
+def timing(func):
+    def _wrapper(*args, **kwargs):
+        start = time()
+        result = func(*args, **kwargs)
+        end = time()
+        print('%s: %s' % (func.__name__, str(end-start)))
+        return result
+    return _wrapper
