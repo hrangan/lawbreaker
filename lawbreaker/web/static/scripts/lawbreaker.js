e82c92b8ff1cab2da3bdd16186e66c3a11a96e2b
Harshavardhan Rangan <hvardhan.r@gmail.com>
2018-12-07 20:45:06 +0530

    trial of custom dropdown

--- lawbreaker/web/static/scripts/lawbreaker.js
+++ lawbreaker/web/static/scripts/lawbreaker.js
@@ -1,11 +1,12 @@
 var progression = JSON.parse(document.getElementById('progression').innerHTML);
 
 function change_level(event_obj) {
-    level = event_obj["currentTarget"]["value"]
+    level = event_obj.target.innerHTML;
     document.getElementById("hit_points").innerHTML = progression[level]['hit_points'] + " / " + progression[level]['hit_points'];
     document.getElementById("xp").innerHTML = progression[level]['xp'];
     document.getElementById("total_slots").innerHTML = progression[level]['attributes']['constitution'];
     document.getElementById("print_level").innerHTML = level;
+    document.getElementById("level").innerHTML = level;
     for ( var attribute in progression[level]['attributes'] ) {
         document.getElementById(attribute + "_defense").innerHTML =
             progression[level]['attributes'][attribute];
@@ -16,8 +17,8 @@ function change_level(event_obj) {
 
 function main() {
     document.addEventListener('DOMContentLoaded', function () {
-      document.getElementById('level_select')
-              .addEventListener('change', change_level);
+      document.getElementById('dropdown_item')
+              .addEventListener('click', change_level);
     });
 }
 
