72fe5a9f67f915978f5f1ffbbb5dc1eecdb53bf1
Harshavardhan Rangan <hvardhan.r@gmail.com>
2020-04-02 16:16:54 +0530

    Simplify the command line usage

--- lawbreaker/knave.py
+++ lawbreaker/knave.py
@@ -5,17 +5,34 @@ from lawbreaker.character import Character
 from lawbreaker.names import Name
 
 
-def main():
-    parser = argparse.ArgumentParser(description="Process some integers.")
-    parser.add_argument("--level", dest="level", action="store", type=int, default=1,
-                        choices=range(1, 11))
+class CustomFormatter(argparse.HelpFormatter):
+    def _format_args(self, action, default_metavar):
+        if action.nargs == argparse.ZERO_OR_MORE:
+            get_metavar = self._metavar_formatter(action, default_metavar)
+            result = '[%s]' % get_metavar(1)
+            return result
+        else:
+            return super()._format_args(action, default_metavar)
 
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument('--random-name', action='store_const', dest='randomName', const=True)
-    group.add_argument("name", action="store", default=["_"*20], nargs='*')
+    def _metavar_formatter(self, action, default_metavar):
+        if action.choices is not None:
+            result = '{%s - %s}' % (action.choices[0], action.choices[-1])
+            return lambda tuple_size: (result, ) * tuple_size
+        else:
+            return super()._metavar_formatter(action, default_metavar)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Create a random character for the Knave roleplaying game",
+                                     formatter_class=CustomFormatter)
+    parser.add_argument("--level", dest="level", action="store", type=int, default=1,
+                        choices=range(1, 11), help='select a character level (optional)')
+
+    parser.add_argument('name', action='store', default=[], nargs='*',
+                        help='name your character (optional)')
 
     args = parser.parse_args()
-    char = Character(name=args.randomName and Name.get() or " ".join(args.name), level=args.level)
+    char = Character(name=' '.join(args.name) or Name.get(), level=args.level)
     print(char)
 
 
